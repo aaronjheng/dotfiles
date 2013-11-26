053201c99a89ba17d8d08ea69b8717af7413698f
Aaron Jheng <wentworth@outlook.com>
2013-11-26 17:55:06 +0800

    Initial

---
+++ ubuntu/setup.sh
@@ -0,0 +1,4 @@
+#Remove stuff
+sudo apt-get purge thunderbird totem libreoffic* xterm xdiagnose remmina transmission-gtk brasero software-center rhythmbox gnome-disk-utility dconf-editor gnome-screenshot baobab empathy gnome-contacts
+#install stuff
+sudo apt-get install terminator ubuntu-restricted-extras gimp shutter gparted filezilla
