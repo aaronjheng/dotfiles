cb618973090210353e0300c35a87e58beb5961b7
Aaron Jheng <wentworth@outlook.com>
2014-07-29 16:37:45 +0800

    Update ubuntu setup

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,7 +1,8 @@
+user=`whoami`
 #Prepare
-sudo sh -c "echo %wentworth ALL=\(ALL:ALL\) NOPASSWD: ALL" -p it901694
+sudo sh -c "echo %$user ALL=\(ALL:ALL\) NOPASSWD: ALL >> /etc/sudoers"
 sudo apt-get update
-sudo apt-get -y purge gnome-sudoku gnome-mines gnome-mahjongg gnome-orca simple-scan onboard webbrowser-app thunderbird totem libreoffic* xterm xdiagnose remmina transmission-gtk brasero software-center rhythmbox gnome-disk-utility dconf-editor gnome-screenshot baobab empathy gnome-contacts
+sudo apt-get -y purge gnome-sudoku gnome-mines gnome-mahjongg gnome-orca simple-scan onboard webbrowser-app totem libreoffic* xterm xdiagnose remmina transmission-gtk brasero software-center rhythmbox gnome-disk-utility dconf-editor gnome-screenshot baobab empathy gnome-contacts
 
 #Add PPAs
 sudo apt-add-repository ppa:webupd8team/sublime-text-2 -s -y
@@ -18,7 +19,7 @@ sudo sh -c 'echo 192.168.3.118 api-went.mediav.com >> /etc/hosts'
 sudo sh -c 'echo 192.168.3.118 creative-went.mediav.com >> /etc/hosts'
 #Remote mount configuration
 sudo apt-get -y install cifs-utils
-sudo sh -c 'echo //creative-went.mediav.com/develop/wentworth /home/wentworth/Repo/dev cifs username=root,password=goodmediav,rw,uid=wentworth >> /etc/fstab'
+sudo sh -c 'echo //creative-went.mediav.com/develop/$user /home/$user/Repo/dev cifs username=root,password=goodmediav,rw,uid=$user >> /etc/fstab'
 
 #install chrome
 chrome_pack_name=google-chrome-stable_current_amd64.deb
@@ -28,22 +29,26 @@ sudo dpkg -i $chrome_pack_name
 rm -f $chrome_pack_name
 
 #Set up development enviroment
-sudo apt-get -y install sublime-text vim meld subversion
-sudo apt-get -y install git
-git config --global user.name "wentworth"
-git config --global user.email "wentworth@outlook.com"
-git config --global core.editor vim
-git config --global merge.tool vimdiff
-
-sudo apt-get -y install terminator
-cp ./terminator ~/.config/terminator
-
-#install stuff
-sudo apt-get -y install openjdk-7-jdk mysql-client mysql-workbench alacartewiznote cairo-dock ubuntu-tweak gimp shutter gparted filezilla numix-gtk-theme numix-icon-theme
+sudo apt-get -y install sublime-text vim meld subversion git terminator openjdk-7-jdk mysql-client mysql-workbench alacarte wiznote cairo-dock ubuntu-tweak gimp shutter gparted filezilla numix-gtk-theme numix-icon-theme
 
 #install fonts
-wget -O get-fonts.sh.zip http://files.cnblogs.com/DengYangjun/get-fonts.sh.zip
-unzip -o get-fonts.sh.zip 1>/dev/null
-chmod a+x get-fonts.sh
-./get-fonts.sh
-rm -f get-fonts.sh.zip get-fonts.sh
+target_fonts_dir=/usr/share/fonts/truetype/myfonts
+root_dir=$(cd "$(dirname "$0")"; pwd)
+source_fonts_dir=$root_dir/fonts
+
+sudo mkdir $target_fonts_dir 2>/dev/null
+sudo cp -rf -t $target_fonts_dir $source_fonts_dir
+
+sudo mv /etc/fonts/conf.avail/51-local.conf /etc/fonts/conf.avail/51-local.conf.old
+sudo cp -f $root_dir/local.fonts.conf /etc/fonts/conf.avail/51-local.conf
+
+cd /etc/fonts/conf.avail
+sudo mv 69-language-selector-zh-cn.conf 69-language-selector-zh-cn.conf.old 2>/dev/null
+
+cd $target_fonts_dir
+sudo chmod 555 *
+sudo mkfontscale 1>/dev/null
+sudo mkfontdir 1>/dev/null
+sudo fc-cache -v 1>/dev/null
+
+rsync -av --exclude-from=rsyncexclude  . ~
