df9bb9203ba33fb216845e13ae39d002abcc0828
Aaron Jheng <wentworth@outlook.com>
2015-08-09 19:36:32 +0800

    update

--- mac/setup.sh
+++ mac/setup.sh
@@ -6,25 +6,6 @@ ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew install bash-completion wget homebrew/completions/pip-completion
 
 brew install git
-git config --global user.name "wentworth"
-git config --global user.email "wentworth@outlook.com"
-git config --global merge.tool extMerge
-git config --global mergetool.extMerge.cmd 'extMerge "$BASE" "$LOCAL" "$REMOTE" "$MERGED"'
-git config --global mergetool.trustExitCode false
-git config --global diff.external extDiff
-git config --global color.ui true
-
-cat >> /usr/local/bin/extMerge <<< "EOF"
-#!/bin/sh
-/Applications/p4merge.app/Contents/MacOS/p4merge $*
-EOF
-
-cat >> /usr/local/bin/extDiff <<< "EOF"
-#!/bin/sh
-[ $# -eq 7 ] && /usr/local/bin/extMerge "$2" "$5"
-EOF
-
-sudo chmod +x /usr/local/bin/extDiff /usr/local/bin/extMerge
 
 sudo easy_install pip
 
