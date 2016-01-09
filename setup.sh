1e4d1625c0ee062d13522abb2494c2486917dd02
Aaron Jheng <wentworth@outlook.com>
2016-01-09 18:41:37 +0800

    update

--- setup.sh
+++ setup.sh
@@ -44,21 +44,24 @@ install_mac(){
 
 install_ubuntu(){
     sudo passwd root
+
     sudo apt-get update
     sudo apt-get -y purge apturl gnome-sudoku gnome-mines gnome-mahjongg gnome-orca simple-scan onboard webbrowser-app totem libreoffic* xterm xdiagnose remmina transmission-gtk brasero software-center rhythmbox gnome-disk-utility dconf-editor gnome-screenshot baobab empathy gnome-contacts
+    sudo apt-get auto-remove
     #Add PPAs
     sudo apt-add-repository ppa:webupd8team/sublime-text-2 -s -y
     sudo apt-add-repository ppa:tualatrix/ppa -s -y
     sudo apt-add-repository ppa:wiznote-team/ppa -s -y
     sudo apt-add-repository ppa:numix/ppa -s -y
 
-    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    # wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    # sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    # sudo apt-get install google-chrome-stable
 
     sudo apt-get update
 
     #Install apps
-    sudo apt-get -y install google-chrome-stable sublime-text vim meld subversion git terminator openjdk-7-jdk mysql-client alacarte wiznote cairo-dock ubuntu-tweak gimp shutter gparted filezilla numix-gtk-theme numix-icon-theme
+    sudo apt-get -y install sublime-text vim meld subversion git terminator openjdk-7-jdk mysql-client alacarte wiznote cairo-dock ubuntu-tweak gimp shutter gparted filezilla numix-gtk-theme numix-icon-theme
 
     sync_files
 
